3f778c09ea1eb1ef0cdcf9fea01a216ff905b292
naman-bruno <naman@usebruno.com>
2025-01-29 13:57:30 +0530

    new table structure with undo/redo

--- packages/bruno-app/src/components/RequestPane/QueryParams/index.js
+++ packages/bruno-app/src/components/RequestPane/QueryParams/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import get from 'lodash/get';
 import cloneDeep from 'lodash/cloneDeep';
 import InfoTip from 'components/InfoTip';
@@ -10,14 +10,16 @@ import {
   updateQueryParam,
   deleteQueryParam,
   moveQueryParam,
-  updatePathParam
+  updatePathParam,
+  redoChange,
+  undoChange
 } from 'providers/ReduxStore/slices/collections';
 import SingleLineEditor from 'components/SingleLineEditor';
 import { saveRequest, sendRequest } from 'providers/ReduxStore/slices/collections/actions';
-
 import StyledWrapper from './StyledWrapper';
 import Table from 'components/Table/index';
 import ReorderTable from 'components/ReorderTable';
+import { uuid } from 'utils/common/index';
 
 const QueryParams = ({ item, collection }) => {
   const dispatch = useDispatch();
@@ -25,14 +27,24 @@ const QueryParams = ({ item, collection }) => {
   const params = item.draft ? get(item, 'draft.request.params') : get(item, 'request.params');
   const queryParams = params.filter((param) => param.type === 'query');
   const pathParams = params.filter((param) => param.type === 'path');
+  const editorRefs = useRef({});
+  const [undoRedoAction, setUndoRedoAction] = useState(null);
+  const previousQueryParamsRef = useRef(queryParams);
+
+
 
   const handleAddQueryParam = () => {
+    const id = uuid()
     dispatch(
       addQueryParam({
         itemUid: item.uid,
+        uid: id,
         collectionUid: collection.uid
       })
     );
+    setTimeout(() => {
+      editorRefs.current[id]?.nameEditor?.focus();
+    }, 0);
   };
 
   const onSave = () => dispatch(saveRequest(item.uid, collection.uid));
@@ -92,16 +104,109 @@ const QueryParams = ({ item, collection }) => {
       })
     );
   };
-
-  const handleRemoveQueryParam = (param) => {
+  const handleRemoveQueryParam = (paramToRemove) => {
     dispatch(
       deleteQueryParam({
-        paramUid: param.uid,
+        paramUid: paramToRemove.uid,
         itemUid: item.uid,
-        collectionUid: collection.uid
+        collectionUid: collection.uid,
       })
     );
+  
+    const newQueryParams = queryParams.filter(
+      (param) => param.uid !== paramToRemove.uid
+    );
+    const prevQueryParams = previousQueryParamsRef.current;
+  
+    setTimeout(() => {
+      focusNearestAvailableEditor(
+        paramToRemove.uid,
+        newQueryParams,
+        prevQueryParams
+      );
+    }, 0);
   };
+  
+
+  function handleUndo(paramUid, cursor) {
+    setUndoRedoAction({ action: 'undo', paramUid, cursor });
+    dispatchUndoRedo('undo', paramUid);
+  }
+  
+  function handleRedo(paramUid, cursor) {
+    setUndoRedoAction({ action: 'redo', paramUid, cursor });
+    dispatchUndoRedo('redo', paramUid);
+  }
+  
+  function dispatchUndoRedo(type, paramUid) {
+    ['request.url', 'request.params'].forEach((draftItem) => {
+      dispatch(
+        type === 'undo' ? undoChange({ itemUid: item.uid, collectionUid: collection.uid, fieldName: draftItem }) :
+        redoChange({ itemUid: item.uid, collectionUid: collection.uid, fieldName: draftItem })
+      );
+    });
+  }
+  
+
+  function focusNearestAvailableEditor(paramUid, newQueryParams, prevQueryParams) {
+    const prevParamIndex = prevQueryParams.findIndex((param) => param.uid === paramUid);
+    let paramIndex = prevParamIndex;
+  
+    if (paramIndex === -1) {
+      paramIndex = newQueryParams.length - 1;
+    }
+  
+    let offset = 0;
+    while (offset < newQueryParams.length) {
+      const upIndex = paramIndex - offset;
+      const downIndex = paramIndex + offset;
+  
+      if (upIndex >= 0 && focusEditorAtIndex(upIndex)) return;
+      if (downIndex < newQueryParams.length && focusEditorAtIndex(downIndex)) return;
+  
+      offset++;
+    }
+  }
+  
+  
+  function focusEditorAtIndex(index) {
+    const param = queryParams[index];
+    if (param && editorRefs.current[param.uid]?.nameEditor) {
+      editorRefs.current[param.uid].nameEditor.focus();
+      return true;
+    }
+    return false;
+  }
+
+  useEffect(() => {
+    if (undoRedoAction) {
+      const { paramUid, cursor } = undoRedoAction;
+      const prevQueryParams = previousQueryParamsRef.current;
+  
+      setTimeout(() => {
+        const newQueryParams = queryParams;
+        previousQueryParamsRef.current = newQueryParams;
+  
+        const addedParams = newQueryParams.filter(
+          (np) => !prevQueryParams.some((pp) => pp.uid === np.uid)
+        );
+  
+        if (addedParams.length > 0) {
+          const addedParamUid = addedParams[0].uid;
+          editorRefs.current[addedParamUid]?.nameEditor?.focus();
+        } else if (editorRefs.current[paramUid]?.nameEditor) {
+          editorRefs.current[paramUid].nameEditor.focus(cursor);
+        } else {
+          focusNearestAvailableEditor(paramUid, newQueryParams, prevQueryParams);
+        }
+  
+        setUndoRedoAction(null);
+      }, 0);
+    }  
+  
+    previousQueryParamsRef.current = queryParams;
+  }, [queryParams, undoRedoAction]);
+  
 
   const handleQueryParamDrag = ({ updateReorderedItem }) => {
     dispatch(
@@ -127,46 +232,87 @@ const QueryParams = ({ item, collection }) => {
           <ReorderTable updateReorderedItem={handleQueryParamDrag}>
             {queryParams && queryParams.length
               ? queryParams.map((param, index) => (
-                  <tr key={param.uid} data-uid={param.uid}>
-                    <td className="flex relative">
+                <tr key={param.uid} data-uid={param.uid}>
+                  <td>
+                    <SingleLineEditor
+                      ref={(el) => {
+                        if (!editorRefs.current[param.uid]) {
+                          editorRefs.current[param.uid] = {};
+                        }
+                        if (el) {
+                          editorRefs.current[param.uid].nameEditor = el;
+                        } else {
+                          delete editorRefs.current[param.uid]?.nameEditor;
+                        }
+                      }}
+                      value={param.name}
+                      theme={storedTheme}
+                      onSave={onSave}
+                      onChange={(newValue) =>
+                        handleQueryParamChange(
+                          { target: { value: newValue } },
+                          param,
+                          'name'
+                        )
+                      }
+                      onRun={handleRun}
+                      collection={collection}
+                      item={item}
+                      variablesAutocomplete={false}
+                      dispatch={dispatch}
+                      draftTimeline={['request.url', 'request.params']}
+                      onUndo={handleUndo}
+                      onRedo={handleRedo}
+                    />
+                  </td>
+                  <td>
+                    <SingleLineEditor
+                      ref={(el) => {
+                        if (!editorRefs.current[param.uid]) {
+                          editorRefs.current[param.uid] = {};
+                        }
+                        if (el) {
+                          editorRefs.current[param.uid].valueEditor = el;
+                        } else {
+                          delete editorRefs.current[param.uid]?.valueEditor;
+                        }
+                      }}
+                      value={param.value}
+                      theme={storedTheme}
+                      onSave={onSave}
+                      onChange={(newValue) =>
+                        handleQueryParamChange(
+                          { target: { value: newValue } },
+                          param,
+                          'value'
+                        )
+                      }
+                      onRun={handleRun}
+                      collection={collection}
+                      item={item}
+                      variablesAutocomplete={false}
+                      dispatch={dispatch}
+                      draftTimeline={['request.url', 'request.params']}
+                      onUndo={handleUndo}
+                      onRedo={handleRedo}
+                    />
+                  </td>
+                  <td>
+                    <div className="flex items-center justify-center">
                       <input
-                        type="text"
-                        autoComplete="off"
-                        autoCorrect="off"
-                        autoCapitalize="off"
-                        spellCheck="false"
-                        value={param.name}
-                        className="mousetrap"
-                        onChange={(e) => handleQueryParamChange(e, param, 'name')}
+                        type="checkbox"
+                        checked={param.enabled}
+                        tabIndex="-1"
+                        className="mr-3 mousetrap"
+                        onChange={(e) => handleQueryParamChange(e, param, 'enabled')}
                       />
-                    </td>
-                    <td>
-                      <SingleLineEditor
-                        value={param.value}
-                        theme={storedTheme}
-                        onSave={onSave}
-                        onChange={(newValue) => handleQueryParamChange({ target: { value: newValue } }, param, 'value')}
-                        onRun={handleRun}
-                        collection={collection}
-                        variablesAutocomplete={true}
-                      />
-                    </td>
-                    <td>
-                      <div className="flex items-center justify-center">
-                        <input
-                          type="checkbox"
-                          checked={param.enabled}
-                          tabIndex="-1"
-                          className="mr-3 mousetrap"
-                          onChange={(e) => handleQueryParamChange(e, param, 'enabled')}
-                        />
-                        <button tabIndex="-1" onClick={() => handleRemoveQueryParam(param)}>
-                          <IconTrash strokeWidth={1.5} size={20} />
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
-                ))
+                      <button tabIndex="-1" onClick={() => handleRemoveQueryParam(param)}>
+                        <IconTrash strokeWidth={1.5} size={20} />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))
               : null}
           </ReorderTable>
         </Table>
@@ -200,43 +346,43 @@ const QueryParams = ({ item, collection }) => {
           <tbody>
             {pathParams && pathParams.length
               ? pathParams.map((path, index) => {
-                  return (
-                    <tr key={path.uid}>
-                      <td>
-                        <input
-                          type="text"
-                          autoComplete="off"
-                          autoCorrect="off"
-                          autoCapitalize="off"
-                          spellCheck="false"
-                          value={path.name}
-                          className="mousetrap"
-                          readOnly={true}
-                        />
-                      </td>
-                      <td>
-                        <SingleLineEditor
-                          value={path.value}
-                          theme={storedTheme}
-                          onSave={onSave}
-                          onChange={(newValue) =>
-                            handlePathParamChange(
-                              {
-                                target: {
-                                  value: newValue
-                                }
-                              },
-                              path
-                            )
-                          }
-                          onRun={handleRun}
-                          collection={collection}
-                          item={item}
-                        />
-                      </td>
-                    </tr>
-                  );
-                })
+                return (
+                  <tr key={path.uid}>
+                    <td>
+                      <input
+                        type="text"
+                        autoComplete="off"
+                        autoCorrect="off"
+                        autoCapitalize="off"
+                        spellCheck="false"
+                        value={path.name}
+                        className="mousetrap"
+                        readOnly={true}
+                      />
+                    </td>
+                    <td>
+                      <SingleLineEditor
+                        value={path.value}
+                        theme={storedTheme}
+                        onSave={onSave}
+                        onChange={(newValue) =>
+                          handlePathParamChange(
+                            {
+                              target: {
+                                value: newValue
+                              }
+                            },
+                            path
+                          )
+                        }
+                        onRun={handleRun}
+                        collection={collection}
+                        item={item}
+                      />
+                    </td>
+                  </tr>
+                );
+              })
               : null}
           </tbody>
         </table>
